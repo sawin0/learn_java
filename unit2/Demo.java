98d0875e3a39a83f61a3bf621439d0dde80ac7f9
Sabin Ranabhat <binranabhat@gmail.com>
2026-05-05 13:00:15 +0545

    chore: add break and continue example

---
+++ unit2/Demo.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Demo{
+    public static void main(String[] args) {
+        System.out.println(7 >> 2);
+        // 
+
+        // Arrays.toString(myArray);
+        //0101 = 5
+        //1010 = 10
+    }
+}
